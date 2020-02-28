af66204e4e8ebe38fa23c2d155bf5fee4346613c
jebitok-dev <sharonkosgei4@gmail.com>
2020-02-28 11:29:30 +0300

    Adopt_Pets App

---
+++ src/App.js
@@ -0,0 +1,31 @@
+const Pet = (props) => {
+    return React.createElement("div", null, [
+        React.createElement("h1", null, props.name),
+        React.createElement("h2", null, props.animal),
+        React.createElement("h2", null, props.breed)
+    ]);
+}
+//component class or functional passes return keyword
+const App = () => {
+    return React.createElement("div",null, [
+        React.createElement("h1", null, "Adopt me"),
+        React.createElement(Pet, {
+            name: "Luna",
+            animal: "Dog",
+            breed: "Havanese"
+        }),
+        React.createElement(Pet, {
+            name: "Pepper",
+            animal: "Bird",
+            breed: "Cockatiel"
+        }),
+        React.createElement(Pet, {
+            name: "Doink",
+            animal: "Cat",
+            breed: "Mix"
+        })
+    ])
+}
+//html tags are put in ""
+ReactDOM.render(React.createElement(App), 
+document.getElementById("root"));
