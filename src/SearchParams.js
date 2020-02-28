ea35842c7cdb804bf07858daefb0af657f990eb1
jebitok-dev <sharonkosgei4@gmail.com>
2020-02-28 17:56:13 +0300

    local commit #3

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 //function component
+//replaceLocation
+
 const SearchParams = () => {
-    const location = "Seattle WA";
+const [location, updateLocation] = useState("Seattle, WA")
     return (
         <div className="search-params">
             <form>
                 <label htmlFor="location">
                     location
-                    <input id="location" value={location}
-                    placeholder="Location" />
+                    <input
+                        id="location" 
+                        value={location}
+                        placeholder="Location"
+                        onChange={(e) => updateLocation
+                        (e.target.value)}
+                     />
                 </label>
                 <button>Submit</button>
             </form>
