8ca8409ff88d11cd41d6dd6817d077ed63d33662
jebitok-dev <sharonkosgei4@gmail.com>
2020-03-02 14:39:17 +0300

    refactor code

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,16 +1,18 @@
 import React, {useState} from 'react';//useState-hook
 import { ANIMALS } from '@frontendmasters/pet'
+import dropDown from './dropdown'
 //function component
 const SearchParams = () => {
     //replace location
     //location represents current state
     //updateLocation -> function to update state
 const [location, updateLocation] = useState("Seattle, WA");
-const [animal, updateAnimal] = useState("");//animal-current state updateAnimal-fn to change state
-const [breed, setBreed]= useState("");
-const [breeds, updateBreeds] = useState([]);
+const [breeds, updateBreeds] = useState([]);//animal-current state updateAnimal-fn to change state
+const [animal, AnimalDropdown]= dropDown("Animal", "dog", ANIMALS);
+const [breed, BreedDropdown] = dropDown("Breed", "", breeds);
     return (
         <div className="search-params">
+            {name}
             <form>
                 <label htmlFor="location">
                     Location
@@ -22,37 +24,12 @@ const [breeds, updateBreeds] = useState([]);
                         (e.target.value)}//target-input, value-location
                     />
                 </label>
-                <label htmlFor="animal">
-                    Animal
-                    <select //comes with dropdown option
-                        id="animal"
-                        value={animal}
-                        onChange={(e) => updateAnimal(e.target.value)} //making change on animal
-                        onBlur={(e) => updateAnimal(e.target.value)}//target-select, value-animal
-                    >
-                        {ANIMALS.map(animal => {//.map whatever you choose from select options
-                            return <option key={animal}>{animal} ></option>
-                        })}
-                    </select>
-                </label>
-                <label htmlFor="breed">
-                    Breed
-                    <select 
-                    disable={!breeds.length}
-                    id="breed"
-                    value={breed}
-                    onChange={(e)=> updateBreeds(e.target.value)}
-                    >
-                        <option />
-                        {breeds.map((breed) => {
-                            return<option key={breed} value={breed}>{breed}</option>
-                        })}
-                    </select>
-                </label>
+                <AnimalDropdown/>
+                <BreedDropdown/>
                 <button>Submit</button>
             </form>
         </div>
-    )
-} 
+    );
+};
 
 export default SearchParams;
