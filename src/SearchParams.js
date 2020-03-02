94d95fae1ff8b987c697da8ed2d21c2868469af9
jebitok-dev <sharonkosgei4@gmail.com>
2020-03-02 10:45:33 +0300

    local commit #4

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
+import { ANIMALS } from '@frontendmasters/pet'
 //function component
 //replaceLocation
-
 const SearchParams = () => {
     //location represents current state
     //updateLocation -> function to update state
-const [location, updateLocation] = useState("Seattle, WA")
+const [location, updateLocation] = useState("Seattle, WA");
+const [animal, updateAnimal] = useState("");//animal-current state updateAnimal-fn to change state
     return (
         <div className="search-params">
             <form>
@@ -18,6 +19,19 @@ const [location, updateLocation] = useState("Seattle, WA")
                         onChange={(e) => updateLocation//event takes callback function
                         (e.target.value)}
                      />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select
+                        id="animal"
+                        value={animal}
+                        onChange={(e) => updateAnimal(e.target.value)} //making change on animal
+                        onBlur={(e) => updateAnimal(e.target.value)}
+                    >
+                        {ANIMALS.map(animal => {
+                            return <option value={animal}>{animal} ></option>
+                        })}
+                    </select>
                 </label>
                 <button>Submit</button>
             </form>
