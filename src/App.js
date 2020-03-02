51e52e71189ade3c474476502db78ac4760bb9f6
jebitok-dev <sharonkosgei4@gmail.com>
2020-03-02 11:49:08 +0300

    local commit #4

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SearchParams from './SearchParams'
 const App = () => {
     return (
         <div>
-            <h1>Adopt Me!</h1>
+            <h1>Adopt Pets!</h1>
             <SearchParams />
         </div>
     )
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,35 +1,51 @@
-import React, {useState} from 'react';
+import React, {useState} from 'react';//useState-hook
 import { ANIMALS } from '@frontendmasters/pet'
 //function component
-//replaceLocation
 const SearchParams = () => {
+    //replace location
     //location represents current state
     //updateLocation -> function to update state
 const [location, updateLocation] = useState("Seattle, WA");
 const [animal, updateAnimal] = useState("");//animal-current state updateAnimal-fn to change state
+const [breed, setBreed]= useState("");
+const [breeds, updateBreeds] = useState([]);
     return (
         <div className="search-params">
             <form>
                 <label htmlFor="location">
-                    location
+                    Location
                     <input
                         id="location" 
                         value={location}
                         placeholder="Location"
-                        onChange={(e) => updateLocation//event takes callback function
-                        (e.target.value)}
-                     />
+                        onChange={(e) => updateLocation//event attribute - callback function
+                        (e.target.value)}//target-input, value-location
+                    />
                 </label>
                 <label htmlFor="animal">
                     Animal
-                    <select
+                    <select //comes with dropdown option
                         id="animal"
                         value={animal}
                         onChange={(e) => updateAnimal(e.target.value)} //making change on animal
-                        onBlur={(e) => updateAnimal(e.target.value)}
+                        onBlur={(e) => updateAnimal(e.target.value)}//target-select, value-animal
                     >
-                        {ANIMALS.map(animal => {
-                            return <option value={animal}>{animal} ></option>
+                        {ANIMALS.map(animal => {//.map whatever you choose from select options
+                            return <option key={animal}>{animal} ></option>
+                        })}
+                    </select>
+                </label>
+                <label htmlFor="breed">
+                    Breed
+                    <select 
+                    disable={!breeds.length}
+                    id="breed"
+                    value={breed}
+                    onChange={(e)=> updateBreeds(e.target.value)}
+                    >
+                        <option />
+                        {breeds.map((breed) => {
+                            return<option key={breed} value={breed}>{breed}</option>
                         })}
                     </select>
                 </label>
